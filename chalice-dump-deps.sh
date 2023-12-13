8bac2df126308ea8549eac3dd620a5274dac2f5e
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2023-12-13 12:44:46 -0600

    Delete chalice-dump-deps.sh

--- chalice-dump-deps.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-pipenv requirements > requirements.txt \
-&& docker run --rm --mount type=bind,source="$(pwd)",target=/var/task public.ecr.aws/sam/build-python3.11 /bin/sh -c "pip install -r requirements.txt -t chalice/vendor/python" \
-&& rm requirements.txt
